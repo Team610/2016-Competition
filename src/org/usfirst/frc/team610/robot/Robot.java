7a07117b111389dc0a5c77054c1932d75086ddfa
Charles Ju <charlesju@crescentschool.org>
2016-01-26 17:06:03 -0500

    j

--- src/org/usfirst/frc/team610/robot/Robot.java
+++ src/org/usfirst/frc/team610/robot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+import org.usfirst.frc.team610.robot.commands.A_Turn;
 import org.usfirst.frc.team610.robot.commands.ExampleCommand;
 import org.usfirst.frc.team610.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -33,7 +35,7 @@ public class Robot extends IterativeRobot {
 
     	chooser = new SendableChooser();
         chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
+        chooser.addObject("Turn", new A_Turn());
         SmartDashboard.putData("Auto mode", chooser);
     }
 	
