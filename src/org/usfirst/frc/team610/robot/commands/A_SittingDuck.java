01b9d4983b99836fa83b7d215b1795cd6d7c2e4c
Charles Ju <charlesju@crescentschool.org>
2016-02-04 19:05:54 -0500

    PID is working. SittingDuck locks position with PID

--- src/org/usfirst/frc/team610/robot/commands/A_SittingDuck.java
+++ src/org/usfirst/frc/team610/robot/commands/A_SittingDuck.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team610.robot.constants.PIDConstants;
 import org.usfirst.frc.team610.robot.subsystems.DriveTrain;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -34,12 +35,17 @@ public class A_SittingDuck extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	tAngle = driveTrain.getYaw();
     	error = 0 - tAngle;
     	leftSpeed = error * PIDConstants.Kp;
     	rightSpeed = error * PIDConstants.Kp;
     	
     	driveTrain.setLeft(-leftSpeed);
     	driveTrain.setRight(rightSpeed);
+    	
+    	SmartDashboard.putNumber("Left Speed: ", leftSpeed);
+    	SmartDashboard.putNumber("Right Speed: ", rightSpeed);
+    	SmartDashboard.putNumber("Error: ", error);
     }
 
     // Make this return true when this Command no longer needs to run execute()
